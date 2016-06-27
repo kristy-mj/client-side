b507ee4bcbfa518aeaafe4860d7bc29fc133b044
Kristy <email": null>
2016-06-28 11:54:55 +1200

    Updates

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 var xhr = require('xhr')
 var greeting = require('./views/greeting.hbs')
+var issData = ('./views/issData.hbs')
 
-var endpoint = 'https://api.wheretheiss.at/v1/coordinates/37.795517,-122.393693'
+var endpoint = 'https://api.wheretheiss.at/v1/satellites'
 
 xhr.get(endpoint, function (err, data) {
   if (err) {
     console.error(err)
   }
-  console.log(data.body) // FYI: data.body is a string
 
-  var satellite = JSON.parse(data.body)
-  console.log(satellite[0])
+  // In case you're curious
+  console.log(JSON.parse(data.body)) // FYI: data.body is a string
 
   // Replace 'Space' below with the response
   var target = document.getElementsByTagName('main')[0]
-  target.innerHTML = greeting({name: 'Space', data: satellite})
+  target.innerHTML = greeting({name: 'Space'})
 })
