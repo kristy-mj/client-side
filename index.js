6908d63ccc32ef9e0a301ffedb381e8f19d20e78
Kristy <email": null>
2016-06-28 16:39:46 +1200

    button included

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var xhr = require('xhr')
 var greeting = require('./views/greeting.hbs')
+var satellite = require('./views/satellite.hbs')
 
 var endpoint = 'https://api.wheretheiss.at/v1/satellites'
 
@@ -7,11 +8,17 @@ xhr.get(endpoint, function (err, data) {
   if (err) {
     console.error(err)
   }
-
-  // In case you're curious
-  console.log(data.body) // FYI: data.body is a string
-
-  // Replace 'Space' below with the response
+  var satObj = JSON.parse(data.body)[0] 
+  
   var target = document.getElementsByTagName('main')[0]
   target.innerHTML = greeting({name: 'Space'})
+
+  var findData = document.getElementsByTagName('button')[0]
+  var data = document.getElementById('data')
+
+  findData.addEventListener('click', function(){
+  	data.innerHTML = satellite(satObj)
+  })
 })
+
+
